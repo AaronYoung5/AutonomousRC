728a596a28f75f40ac8b35531390ec662ecc3193
Aaron Young <aryoung5@wisc.edu>
2019-08-30 17:05:58 +0000

    ike saving

--- catkin_ws/src/control/cone_map/circle_through_3_cones/src/controller.cpp
+++ catkin_ws/src/control/cone_map/circle_through_3_cones/src/controller.cpp
@@ -3,12 +3,12 @@
 
 // subscribes to a cone_map topic, publishes to control  topic.
 Controller::Controller(ros::NodeHandle &n) {
-  std::string cone_map_topic, control_topic;
-  n.param<std::string>("cone_map_topic", cone_map_topic, "cone_map");
+  std::string cone_topic, control_topic;
+  n.param<std::string>("cone_topic", cone_topic, "cone_map");
   n.param<std::string>("control_topic", control_topic, "control");
 
   pub_ = n.advertise<common_msgs::Control>(control_topic, 1);
-  sub_ = n.subscribe(cone_map_topic, 1, &Controller::mapCallback, this);
+  sub_ = n.subscribe(cone_topic, 1, &Controller::mapCallback, this);
 }
 
 // TODO: not sure this does the right thing, need it to take in mesages from
